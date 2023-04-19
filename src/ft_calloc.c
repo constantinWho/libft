1d85f0b75f2b0aa428c38a25d6677b1837d7e520
chustei <chustei@student.42berlin.de>
2023-04-19 13:12:42 +0200

    merge of libft+get_next_line+ft_printf

---
+++ src/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chustei <chustei@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 14:04:54 by chustei           #+#    #+#             */
+/*   Updated: 2023/03/02 12:57:40 by chustei          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*p;
+
+	p = malloc(nmemb * size);
+	if (!p)
+		return (0);
+	ft_bzero(p, nmemb * size);
+	return (p);
+}
